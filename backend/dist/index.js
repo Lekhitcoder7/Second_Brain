270c0397ef382d61ab31ea13fcbf57ecde4a2412
Lakshay <lekhitsanduja@gmail.com>
2025-12-17 22:30:04 +0530

    Fix MongoDB env and server start issue

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -11,9 +11,20 @@ const mongoose = require("mongoose");
 const auth_route_1 = __importDefault(require("./Routes/auth.route"));
 const content_route_1 = __importDefault(require("./Routes/content.route"));
 const brain_route_1 = __importDefault(require("./Routes/brain.route"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
+
+
 const app = (0, express_1.default)();
 app.use(cors());
-mongoose.connect(process.env.MONGODB_URL);
+mongoose.connect(process.env.MONGODB_URI);
+moongoose.then(() => console.log("MongoDB Connected Successfully"))
+moongoose.catch(err => console.log("MongoDB Error:", err));
 app.use(express_1.default.json());
 app.use("/api/v1/auth", auth_route_1.default);
 app.use("/api/v1/content", content_route_1.default);
